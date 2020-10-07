e3480bc9619109bcc8ab2988303ccaf653b21e64
Bruno Simm <56409507+BrunoSimm@users.noreply.github.com>
2020-10-07 19:28:48 -0300

    Ex4

--- Exercicio11_ClassesAbstratas-4/App.java
+++ Exercicio11_ClassesAbstratas-4/App.java
@@ -7,7 +7,8 @@ public class App {
     public static void main(String[] args) {
         Embalagem e1 = new EmbalagemTroncoCone(1, "A1");
         System.out.println("Volume: "+((EmbalagemTroncoCone)e1).getVolume(5, 2, 4));
-        System.out.println(verificaDiferenciada(e1));
         System.out.println(e1);
+        System.out.println("Embalagem Diferenciada: "+ verificaDiferenciada(e1));
+
     }
 }
