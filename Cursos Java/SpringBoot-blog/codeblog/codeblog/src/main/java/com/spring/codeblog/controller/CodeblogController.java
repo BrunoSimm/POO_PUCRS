e5350c23840f5f051c4f60c9baf375e730834ad9
Bruno Simm <56409507+BrunoSimm@users.noreply.github.com>
2021-03-07 15:41:59 -0300

    Implementando criação de novos POST e salvando no db.

--- Cursos Java/SpringBoot-blog/codeblog/codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ Cursos Java/SpringBoot-blog/codeblog/codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -1,13 +1,18 @@
 package com.spring.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
@@ -39,8 +44,23 @@ public class CodeblogController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm(){
+		//"Quando uma requisição do tipo GET for feita para /newpost deve-se retornar a página html postForm.
+		return "postForm.html";
+	}
 	
-	
-	
-	
+	//Quando uma requisição do tipo POST for feita na página /newpost este método será executado.
+	//Campos "name" no formulário html de requisição devem ser iguais aos declarados na classe Post (titulo, autor, texto).
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		
+		post.setData(LocalDate.now());
+		codeBlogService.save(post);
+		return "redirect:/posts";
+	}
+
 }
